27541e9b916cfe1436039ff7217fd85a654eb8f5
manasakancharla1 <126057913+manasakancharla1@users.noreply.github.com>
2024-07-20 17:43:27 +0530

    Update db.php

--- transport managment system/db.php
+++ transport managment system/db.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "manasa@0603";
-$dbname = "transport_managment";
+$password = "";
+$dbname = "";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
